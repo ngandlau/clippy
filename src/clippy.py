a3b38fb59972f1eff599d353eebf8a493285fba5
ngormsen <nils.gormsen@googlemail.com>
2022-07-16 10:49:10 +0200

    start to implement attack feature

--- src/clippy.py
+++ src/clippy.py
@@ -1,11 +1,25 @@
+from __future__ import annotations
+
 import math
 
+
 class Clippy():
+    def __init__(self):
+        self.weapon = None
+        self.life = 10
+
+    def equip_weapon(self, weapon: Weapon):
+        self.weapon = weapon
+
     def greet(self):
         return "Hello Nils, I'm Clippy!"
 
     def farewell(self):
         return "Goodbye, old friend!"
+
+    def attack(self, other: Clippy):
+        other.life - self.weapon.get_base_attack()
+
 
 if __name__ == '__main__':
     print('hello world!')
--- testing/test_clippy.py
+++ testing/test_clippy.py
@@ -7,3 +7,11 @@ def test_clippy_can_greet():
 def test_clippy_can_farewell():
     clpy = clippy.Clippy()
     assert clpy.farewell() == "Goodbye, old friend!"
+
+def test_clippy_can_attack():
+    gormsen = clippy.Clippy()
+    gandlau = clippy.Clippy()
+    gandlau_life_before = gandlau.life
+    gormsen.attack(gandlau)
+    assert gandlau.life < gandlau_life_before
+    assert gandlau.life == gandlau_life_before - 1
