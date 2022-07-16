cfc159d8485ffa5dd64ed4b3a25bc72efe08fb73
Nils Gandlau <28842354+ngandlau@users.noreply.github.com>
2022-07-16 09:48:37 +0200

    setup pytest

--- clippy.py
+++ clippy.py
@@ -1,7 +1,11 @@
 import math
 
-def my_function():
-    None
+def add(a, b):
+    return a + b
+
+class MyClass:
+    def __init__(self):
+        None
 
 if __name__ == '__main__':
     print('hello world!')
---
+++ testing/test_clippy.py
@@ -0,0 +1,4 @@
+import clippy
+
+def test_add():
+    assert clippy.add(5, 5) == 10
